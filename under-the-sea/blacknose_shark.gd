45aef91cb69bd8b30fdb4e66b673d8a930f32d09
Maryam <135457385+maryamalateye@users.noreply.github.com>
2025-12-15 19:01:01 +0000

    Added label to have information on

--- under-the-sea/blacknose_shark.gd
+++ under-the-sea/blacknose_shark.gd
@@ -2,8 +2,8 @@ extends XRToolsPickable
 
 @onready var rig := $SharkRig
 
-@export var orbit_radius: float = 3.0
-@export var orbit_speed: float = 0.3   #turning speed
+@export var orbit_radius: float = 10.0
+@export var orbit_speed: float = 0.08   #turning speed
 @export var swim_speed: float = 0.8    #actual swimming speed
 
 var _swimming := true	#initially swimming
@@ -19,18 +19,12 @@ func _ready():
 func _process(delta):
 	if not _swimming:
 		return
-
 	
 	rig.rotate_y(orbit_speed * delta)
 	rig.translate(Vector3.FORWARD * swim_speed * delta)
 
 	var to_home = rig.global_position - home_position
 	var dist = to_home.length()
-#
-	#if dist > orbit_radius:
-		## Push shark slightly inward so it stays roughly in a wide circle
-		#var inward = -to_home.normalized()
-		#rig.global_translate(inward * (dist - orbit_radius) * 0.02)
 
 func _on_picked_up(_p):
 	_swimming = false
